307561c33eae4711550458d91dd1a27ab2ca7e6e
zjx <zjx>
2017-11-08 10:08:15 +0800

    修改了tsung测试的脚本

--- tsung/pre-work.sh
+++ tsung/pre-work.sh
@@ -1,7 +1,4 @@
 #bin/sh
-echo "10.32.0.5 foo.bar.com" >> /etc/hosts;
-echo "10.32.0.5 bar.baz.com" >> /etc/hosts;
-
-echo "haha"
+echo "10.40.0.2 test.java.com" >> /etc/hosts;
 
 tsung -f /root/.tsung/tsung-test.xml start;
